59d8b4096c27621807fbfde8a8888114387fa79e
andychicken <83952965+andychicken@users.noreply.github.com>
2021-05-31 16:31:11 +0700

    add mung-express

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const todoRouter = require("./src/todoRouter");
+const mung= require('express-mung');
 
 const app = express();
 
 // Add body-parser middleware to automatically parse JSON requests
 app.use(bodyParser.json());
 // Requests done to the base path /todo should go through the todoRouter
-app.use("/todo", todoRouter);
+app.use(mung.json(
 
-const startApplication = async () => {
-  await mongoose.connect("mongodb+srv://andy:andy123@cluster0.ixw6l.mongodb.net/todos?retryWrites=true&w=majority", {
+  (body,req,res)=>{
+    const body2={
+      'res' : 'ok',
+      'data':body}
+   return body2;
+  }));
+
+app.use("/todos", todoRouter);
+
+const startApplication = async(body) => {
+   
+   await mongoose.connect("mongodb+srv://andy:andy123@cluster0.ixw6l.mongodb.net/todos?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
