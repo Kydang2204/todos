97d2d2d3550f9f8d84594085980cf5fbd9929615
andychicken <83952965+andychicken@users.noreply.github.com>
2021-06-03 09:34:35 +0700

    Keep calm and commit

--- index.js
+++ index.js
@@ -34,9 +34,6 @@ const startApplication = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
 
-
-
-      
     });
   console.log(
     'Successfully connected to MongoDB at: mongodb',
