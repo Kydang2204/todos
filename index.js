f8b458a3c1a016b18cd1b125519fb56d4317e4a9
andychicken <83952965+andychicken@users.noreply.github.com>
2021-06-04 18:25:22 +0700

    áarrange

--- index.js
+++ index.js
@@ -3,47 +3,18 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const mung = require('express-mung');
+const mung = require('./src/mung');
 const todoRouter = require('./src/todoRouter');
 
 const app = express();
-
-// Add body-parser middleware to automatically parse JSON requests
 app.use(bodyParser.json());
-// Requests done to the base path /todo should go through the todoRouter
-app.use(mung.json(
 
-  (body) => {
-    const body2 = {
-
-      res: 'ok',
-      data: body,
-    };
-    return body2;
-  },
-));
+app.use(mung);
 
 app.use('/todos', todoRouter);
 
-const startApplication = async () => {
-  console.log();
-
-  await mongoose.connect(`mongodb+srv://andy:${process.env.PW}@cluster0.ixw6l.mongodb.net/todos?retryWrites=true&w=majority`,
-    {
-
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-
-    });
-
-
-    
-  console.log(
-    'Successfully connected to MongoDB at: mongodb',
-  );
-  console.log();
-  await app.listen(process.env.PORT);
-  console.log(`Listening on port ${process.env.PORT}`);
-};
-
-startApplication();
+mongoose.connect(`mongodb+srv://andy:${process.env.PW}@cluster0.ixw6l.mongodb.net/todos?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+console.log('Successfully connected to MongoDB at: mongodb');
+app.listen(process.env.PORT);
+console.log(`Listening on port ${process.env.PORT}`);
---
+++ src/mung.js
@@ -0,0 +1,17 @@
+const mung = require('express-mung');
+const express = require('express');
+
+const router = express.Router();
+
+router.use(mung.json(
+
+  (body) => {
+    const body2 = {
+      res: 'ok',
+      data: body,
+    };
+    return body2;
+  },
+));
+
+module.exports = router;
